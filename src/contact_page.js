94c047ef94e025f48109e05ddb0a8cab08546311
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-03-27 13:39:33 +0100

    Adds all contect for the contact page

--- src/contact_page.js
+++ src/contact_page.js
@@ -21,10 +21,55 @@ export default class ContactPage {
         this.state = true;
 
         this.contentWrapper = document.querySelector(".content-wrapper");
-
         this.contentContainer = document.createElement("div");
-        this.contentContainer.className = "content contact";
-        this.contentWrapper.appendChild(this.contentContainer);
+        this.contactInfoHeader = document.createElement("h1");
+        this.locationHeader = document.createElement("h2");
+        this.location = document.createElement("p");
+        this.emailHeader = document.createElement("h2");
+        this.email = document.createElement("p");
+        this.phoneHeader = document.createElement("h2");
+        this.phone = document.createElement("p");
+        this.operatingHoursHeader = document.createElement("h2");
+        this.operatingHours = document.createElement("p");
+        this.reservations = document.createElement("p");
+
         this.contentContainer.textContent = "";
+        
+        this.contentContainer.className = "content contact";
+        this.contactInfoHeader.className = "contactInfoHeader";
+        this.locationHeader.className = "locationHeader";
+        this.location.className = "location";
+        this.emailHeader.className = "emailHeader";
+        this.email.className = "email";
+        this.phoneHeader.className = "phone";
+        this.operatingHoursHeader.className = "operatingHoursHeader";
+        this.operatingHours.className = "operatingHours";
+        this.reservations.className = "";
+
+        this.contactInfoHeader.textContent = "Contact Info";
+        this.locationHeader.textContent = "Location:";
+        this.location.textContent = "Aegean Breeze Seaside Promenade, Chania, Crete Greece";
+        this.emailHeader.textContent = "Email:";
+        this.email.textContent = "info@aegeanbreeze.com";
+        this.phoneHeader.textContent = "Phone:";
+        this.phone.textContent = "+30 28210 XXXXX";
+        this.operatingHoursHeader.textContent = "Opening Hours:";
+        this.operatingHours.textContent = "Monday - Sunday: 12:00 PM - 11:00 PM"
+        this.reservations.textContent = "For reservations, inquiries, or feedback, please feel free to contact us via phone or email. We look forward to hearing from you!";
+
+        this.contentContainer.append(
+            this.contactInfoHeader,
+            this.locationHeader,
+            this.location,
+            this.phoneHeader,
+            this.phone,
+            this.emailHeader,
+            this.email,
+            this.operatingHoursHeader,
+            this.operatingHours,
+            this.reservations
+        )
+        this.contentWrapper.appendChild(this.contentContainer);
+        
     }
 }
